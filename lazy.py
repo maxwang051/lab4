27488ed7e6d4bbe208654cca39ecd37a549c86b4
Max Wang <maxwang051@gmail.com>
2016-03-01 17:19:36 -0600

    updated script for better formatting

--- lazy.py
+++ lazy.py
@@ -4,4 +4,4 @@ out = open('formatted.txt', 'w')
 for line in f:
     line = line[9:-3]
     splitLine = [line[i:i+8] for i in range(0, len(line), 8)]
-    out.write(' '.join(splitLine))
+    out.write(' '.join(splitLine) + '\n')
