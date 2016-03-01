5e7eae6466b201cde3e17af255e466109b85fe08
Max Wang <maxwang051@gmail.com>
2016-03-01 17:13:21 -0600

    wrote script to format output

---
+++ lazy.py
@@ -0,0 +1,7 @@
+f = open('data.txt', 'r')
+out = open('formatted.txt', 'w')
+
+for line in f:
+    line = line[9:-3]
+    splitLine = [line[i:i+8] for i in range(0, len(line), 8)]
+    out.write(' '.join(splitLine))
